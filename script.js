9c901b9eb1e026321c81188f5b1506fa514b340c
Roger Tan <mrrogertan@gmail.com>
2022-02-04 13:49:49 +1100

    edited blah

---
+++ script.js
@@ -0,0 +1,34 @@
+function add (number1, number2) {
+    return number1 + number2;
+}
+
+function subtract (number1, number2) {
+    return number1 - number2;
+}
+
+function multiply (number1, number2) {
+    return number1 * number2;
+}
+
+function divide (number1, number2) {
+    return number1 / number2;
+}
+
+
+function operate (operator, number1, number2) {
+    if (operator == "+") {
+        return add (number1, number2)
+    } 
+    else if (operator == "-") {
+        return subtract (number1, number2)
+    }
+    else if (operator == "*") {
+        return multiply (number1, number2)
+    }
+    else if (operator == "divide") {
+        return divide (number1, number2)
+    } 
+    else return "inavlid operator";
+}
+
+console.log(operate("*",1,4));
